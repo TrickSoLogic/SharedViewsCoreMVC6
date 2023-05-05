05c15528824e6c1a211890f42a7423bb5d99bff4
Vishal <vishalbhat.com@gmail.com>
2023-05-05 17:28:55 +0530

    Add project files.

---
+++ ViewsExample/Models/PersonAndProductWrapperModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedViewsCoreMVC6.Models
+{
+  public class PersonAndProductWrapperModel
+  {
+    public Person PersonData { get; set; }
+    public Product ProductData { get; set; }
+  }
+}
---
+++ ViewsExample/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedViewsCoreMVC6.Models
+{
+  public class Product
+  {
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+  }
+}
